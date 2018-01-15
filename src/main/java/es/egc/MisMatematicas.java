a5e5ac7072e9361c06373eae9b391dcdd07476e5
Manuel <manuelgomezsuarez@gmail.com>
2018-01-15 03:54:48 +0100

    Creando conflicto

--- src/main/java/es/egc/MisMatematicas.java
+++ src/main/java/es/egc/MisMatematicas.java
@@ -9,9 +9,9 @@ public class MisMatematicas
 {
 	static Logger log=Logger.getLogger(MisMatematicas.class); 
 	
-	public static int divide(int a, int b){
-		int result= a/b;
-		log.info("Returning "+a+"/"+b+"="+result);
+	public static int divide(int a, int c){
+		int result= a/c;
+		log.info("Devolviendo "+a+"/"+c+"="+result);
 		return result;
 	}
 	
@@ -20,4 +20,5 @@ public class MisMatematicas
 	log.info("Returning "+a+"*"+b+"="+result);
 	return result;
 	}
+	//Hola que pasa
 }
