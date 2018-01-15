3dd558ecb1c098730c720a16243203813ffb1633
Manuel <manuelgomezsuarez@gmail.com>
2018-01-15 03:56:25 +0100

    Cambio cosas

--- src/main/java/es/egc/MisMatematicas.java
+++ src/main/java/es/egc/MisMatematicas.java
@@ -9,9 +9,9 @@ public class MisMatematicas
 {
 	static Logger log=Logger.getLogger(MisMatematicas.class); 
 	
-	public static int divide(int a, int b){
-		int result= a/b;
-		log.info("Returning "+a+"/"+b+"="+result);
+	public static int divide(int a, int x){
+		int result= a/x;
+		log.info("Returning "+a+"/"+x+"="+result);
 		return result;
 	}
 	
@@ -20,4 +20,5 @@ public class MisMatematicas
 	log.info("Returning "+a+"*"+b+"="+result);
 	return result;
 	}
+	//Adios muy buenas
 }
